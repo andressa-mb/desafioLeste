1a50fb54447bee321b200a5e3ff071b0e483aeb7
andressa-mb <andressamesquita26@gmail.com>
2024-07-03 20:35:19 -0300

    Atualização da base_url

--- src/api/ContatoService.js
+++ src/api/ContatoService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://desafio-back-leste.vercel.app";
 
 export class ContatoService{
     static getContatos(){
